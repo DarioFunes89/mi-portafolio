067219dc89bfe64052792cc71984cbbb555cebfc
DarioFunes89 <dariofunes89@gmail.com>
2023-08-25 17:20:56 -0300

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -2,7 +2,6 @@ import React from "react";
 import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 import { RainbowHighlight } from "./RainbowHighlight";
 import userData from "@constants/data";
-import "./Hero.module.css"
 
 export default function Hero() {
   const colors = ["#F59E0B", "#84CC16", "#10B981", "#3B82F6"];
